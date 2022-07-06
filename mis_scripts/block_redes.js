ff4c1cb6a7efec147d86c6ab42664a41f4b6366b
mi nombre <desarrollowebaps@gmail.com>
2022-07-06 16:51:51 -0400

    Actualizando portafolio 16:51

--- mis_scripts/block_redes.js
+++ mis_scripts/block_redes.js
@@ -1,22 +1,27 @@
 let block_redes = document.querySelector("#block_redes")
 let contSkillls = document.querySelector("#habilidades")
+let conPortafolio = document.querySelector("#portafolio")
 
 
 window.document.addEventListener("scroll", () =>{
     const posScrolling = document.documentElement.scrollTop
-    console.log(posScrolling)
+    //console.log(posScrolling)
+
+    const posTitlePortafolio = conPortafolio.offsetTop    //1079
      
     const posBlockRedes = block_redes.offsetTop    //100
-    console.log(posBlockRedes)
+    //console.log(posBlockRedes)
 
     const posSkillls = contSkillls.offsetTop      //4510
-    console.log(posSkillls) 
+    //console.log(posSkillls) 
 
     if(posScrolling < posBlockRedes){
         block_redes.classList.remove("contRedes_js")
     }else if(posScrolling > posBlockRedes + 500){
         block_redes.classList.add("contRedes_js")
     }if(posScrolling > posSkillls){
+        block_redes.classList.remove("contRedes_js")
+    }else if(posScrolling < posTitlePortafolio){
         block_redes.classList.remove("contRedes_js")
     }
 
