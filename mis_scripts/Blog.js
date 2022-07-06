f9acced87636a6c62c48dfedf8532f0311de23c6
mi nombre <desarrollowebaps@gmail.com>
2022-07-05 20:58:44 -0400

    Actualizando portafolio 20:58

--- mis_scripts/Blog.js
+++ mis_scripts/Blog.js
@@ -1,17 +1,27 @@
 let contBlog = document.querySelector("#contBlog")
+let titleSkills = document.querySelector("#habilidades")
 
 
+    
 document.addEventListener("scroll", () =>{
     let positionScroll = document.documentElement.scrollTop
     console.log(positionScroll)
-    let positionElementBlog = contBlog.offsetTop     //296
-    console.log(positionElementBlog)
+    let positionElementBlog = contBlog.offsetTop     //350
+    //console.log(positionElementBlog)
+    let titleSkillsPos = titleSkills.offsetTop   //2905
+    //console.log(titleSkills)
 
-
-    if(positionScroll > positionElementBlog + 2000){
-        contBlog.classList.add("contBlog_JS")
-    }else if(positionScroll < 2400){
+    if(positionScroll < positionElementBlog ){
         contBlog.classList.remove("contBlog_JS")
+        console.log("desaparece")
+    }
+    else if(positionScroll > positionElementBlog){
+        contBlog.classList.add("contBlog_JS")
+        console.log("aparece")
+
+    }if(positionScroll > titleSkillsPos){
+        contBlog.classList.remove("contBlog_JS")
+        console.log("desaparecer de nuevo")  
     }
 })
 
