9f748048b1ce2372acab8cee58687b98737fca91
mi nombre <desarrollowebaps@gmail.com>
2022-07-21 23:43:48 -0400

    Actualizando blog

--- mis_scripts/templates/templates_blog.js
+++ mis_scripts/templates/templates_blog.js
@@ -13,9 +13,23 @@ const dataFechArticle = async ()=>{
     try{
         const resArticle = await fetch("blogArticle.json")
         const dataArticle = await resArticle.json()
+        pintarArticleBlog_1_7(dataArticle)
 
-        pintarArticleBlog(dataArticle)
-
+        document.addEventListener("click", (e) =>{
+            if(e.target.id === "numBlogUno"){
+                contBlogBloggersBase.textContent = pintarArticleBlog_1_7(dataArticle)
+                pintarArticleBlog_1_7(dataArticle)
+                console.log("le diste a uno")
+            }else if(e.target.id === "numBlogdos"){
+                contBlogBloggersBase.textContent = pintarArticleBlog_8_14(dataArticle)
+                pintarArticleBlog_8_14(dataArticle)
+                console.log("le diste a dos")
+            }else if(e.target.id === "numBlogtres"){
+                contBlogBloggersBase.textContent = pintarArticleBlog_15_21(dataArticle)
+                pintarArticleBlog_15_21(dataArticle)
+                console.log("le diste a tres")
+            }
+        })
 
     }catch(error){
         console.log("Error carga article")
@@ -23,10 +37,48 @@ const dataFechArticle = async ()=>{
 }
 
 
-const pintarArticleBlog = (dataArticle)=>{
-    //console.log(dataArticle)
+const pintarArticleBlog_1_7 = (dataArticle)=>{
+    //console.log(dataArticle.blog_1_7)
 
-    dataArticle.forEach( article => {
+    dataArticle.blog_1_7.forEach( article => {
+        //console.log(article.id)
+        template_article.querySelector("#titleBlog").textContent = article.title
+        template_article.querySelector("#parrafoBlog").textContent = article.descripcion
+        template_article.querySelector("#imgBlog").setAttribute("src",article.imagen)
+        template_article.querySelector("#imgBlog").setAttribute("alt",article.title)
+        template_article.querySelector("#btnBlog").dataset.idBlog = article.id
+
+        let cloneBlog = template_article.cloneNode(true)
+        fragmentArticle.appendChild(cloneBlog)
+    });
+    contBlogBloggersBase.appendChild(fragmentArticle)
+
+}
+
+
+const pintarArticleBlog_8_14 = (dataArticle)=>{
+    //console.log(dataArticle.blog_1_7)
+
+    dataArticle.blog_8_14.forEach( article => {
+        //console.log(article.id)
+        template_article.querySelector("#titleBlog").textContent = article.title
+        template_article.querySelector("#parrafoBlog").textContent = article.descripcion
+        template_article.querySelector("#imgBlog").setAttribute("src",article.imagen)
+        template_article.querySelector("#imgBlog").setAttribute("alt",article.title)
+        template_article.querySelector("#btnBlog").dataset.idBlog = article.id
+
+        let cloneBlog = template_article.cloneNode(true)
+        fragmentArticle.appendChild(cloneBlog)
+    });
+    contBlogBloggersBase.appendChild(fragmentArticle)
+
+}
+
+const pintarArticleBlog_15_21 = (dataArticle)=>{
+    //console.log(dataArticle.blog_1_7)
+
+    dataArticle.blog_15_21.forEach( article => {
+        //console.log(article.id)
         template_article.querySelector("#titleBlog").textContent = article.title
         template_article.querySelector("#parrafoBlog").textContent = article.descripcion
         template_article.querySelector("#imgBlog").setAttribute("src",article.imagen)
