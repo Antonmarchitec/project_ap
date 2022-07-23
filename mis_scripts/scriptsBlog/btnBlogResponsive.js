cdc5c5fa094c5f5ebba3c050c4de4d1bdd14e877
mi nombre <desarrollowebaps@gmail.com>
2022-07-23 01:17:46 -0400

    Actualizando blog

---
+++ mis_scripts/scriptsBlog/btnBlogResponsive.js
@@ -0,0 +1,7 @@
+let btnBlogResp = document.querySelector("#btnBlogResp")
+let contBlogMenu_ul = document.querySelector("#contBlogMenu_ul")
+
+
+btnBlogResp.addEventListener("click", () =>{
+    contBlogMenu_ul.classList.toggle("contBlogMenu_ul_JS")
+})
