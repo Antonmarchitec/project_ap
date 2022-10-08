77f14c1cbc2dff2ac3a971a459cbf0043e826844
mi nombre <desarrollowebaps@gmail.com>
2022-10-08 18:51:09 -0400

    06_proyecto_portafolio

--- mis_scripts/templates/template_landing.js
+++ mis_scripts/templates/template_landing.js
@@ -20,39 +20,64 @@ const dataFechWeb = async ()=>{
 }
 
 const pintarlanding = (dataWeb) =>{
-    dataWeb.landing_1_4.forEach(element => {
-        console.log(element.title)
-        templateWeb.querySelector("img").setAttribute("src", element.image)
-        templateWeb.querySelector("img").setAttribute("alt", element.title)
+    dataWeb.landing.forEach(element => {
+        console.log(element.image1)
+        templateWeb.querySelector("#image1").setAttribute("src", element.image1)
+        templateWeb.querySelector("#image1").setAttribute("alt", element.title1)
+        templateWeb.querySelector("#image2").setAttribute("src", element.image2)
+        templateWeb.querySelector("#image2").setAttribute("alt", element.title2)
 
-        templateWeb.querySelector("#webTitle").textContent = element.title
+        
+        templateWeb.querySelector("#webTitle").textContent = element.title1
+        templateWeb.querySelector("#webTitle2").textContent = element.title2
 
-        templateWeb.querySelector("#webDescription").textContent = element.descripcion
+        templateWeb.querySelector("#webDescription").textContent = element.descripcion1
+        templateWeb.querySelector("#webDescription2").textContent = element.descripcion2
 
+        templateWeb.querySelector("#titleProy").textContent = element.subtitleProy
         templateWeb.querySelector("#titleResp").textContent = element.subtitleResp
+        templateWeb.querySelector("#titleResp2").textContent = element.subtitleResp
+
         templateWeb.querySelector("#desktop").textContent = element.dis1
         templateWeb.querySelector("#tablet").textContent = element.dis2
         templateWeb.querySelector("#movil").textContent = element.dis3
+        templateWeb.querySelector("#desktop2").textContent = element.dis1
+        templateWeb.querySelector("#tablet2").textContent = element.dis2
+        templateWeb.querySelector("#movil2").textContent = element.dis3
 
-        templateWeb.querySelector("#titleProy").textContent = element.subtitleProy
-        templateWeb.querySelector("#text1").textContent = element.tip1
-        templateWeb.querySelector("#text2").textContent = element.tip2
-        templateWeb.querySelector("#text3").textContent = element.tip3
-        templateWeb.querySelector("#text4").textContent = element.tip4
+        templateWeb.querySelector("#text1A").textContent = element.tip1A
+        templateWeb.querySelector("#text1B").textContent = element.tip1B
+        templateWeb.querySelector("#text1C").textContent = element.tip1C
+        templateWeb.querySelector("#text1D").textContent = element.tip1D
 
-        templateWeb.querySelector("#btnWeb").dataset.id = element.id
+        templateWeb.querySelector("#text2A").textContent = element.tip1A
+        templateWeb.querySelector("#text2B").textContent = element.tip1B
+        templateWeb.querySelector("#text2C").textContent = element.tip1C
+        templateWeb.querySelector("#text2D").textContent = element.tip2D
 
 
-        const cloneWeb = templateWeb.cloneNode(true)
+        templateWeb.querySelector("#btnWeb1").dataset.id = element.id_block1
+        templateWeb.querySelector("#btnWeb2").dataset.id = element.id_block2
+
+
+        let cloneWeb = templateWeb.cloneNode(true)
         fragmentWeb.appendChild(cloneWeb)
     });
     contTemplateWeb.appendChild(fragmentWeb)
+   
 
 
     //Ejecutar eventos
     document.addEventListener("click", (e)=>{
         if(e.target.dataset.id === "1"){
             window.open("https://antonmarchitec.github.io/landing_restaurant/")
+        }else if(e.target.dataset.id === "2"){
+            window.open("https://antonmarchitec.github.io/landing_veterinaria_vip/")
+        }else if(e.target.dataset.id === "3"){
+            window.open("https://antonmarchitec.github.io/landing_resposteria/")
+        }else if(e.target.dataset.id === "4"){
+            window.open("https://antonmarchitec.github.io/template_gamer/")
         }
+        
     }) 
 }
