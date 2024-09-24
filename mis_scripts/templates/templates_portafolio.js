87a08d5a5f005432b15c738cb5dcb24c70217c76
mi nombre <desarrollowebaps@gmail.com>
2024-09-23 21:45:58 -0400

    142_proyecto_portafolio

--- mis_scripts/templates/templates_portafolio.js
+++ mis_scripts/templates/templates_portafolio.js
@@ -1,5 +1,3 @@
-
-
 let basePortafolio = document.querySelector("#baseContainerNew")
 let templatePortafolio = document.querySelector("#templatePortafolio").content
 let fragmentPortafolio = document.createDocumentFragment()
@@ -52,46 +50,3 @@ const pintarPortafolio = (dataPortafolio)=>{
         }
     })
 }
-
-//FUNCION LANDING
-
-
-
-
-/*
-        templateWeb.querySelector("#image1").setAttribute("src", element.image1)
-        templateWeb.querySelector("#image1").setAttribute("alt", element.title1)
-        templateWeb.querySelector("#image2").setAttribute("src", element.image2)
-        templateWeb.querySelector("#image2").setAttribute("alt", element.title2)
-
-        
-        templateWeb.querySelector("#webTitle").textContent = element.title1
-        templateWeb.querySelector("#webTitle2").textContent = element.title2
-
-        templateWeb.querySelector("#webDescription").textContent = element.descripcion1
-        templateWeb.querySelector("#webDescription2").textContent = element.descripcion2
-
-        templateWeb.querySelector("#titleProy").textContent = element.subtitleProy
-        templateWeb.querySelector("#titleResp").textContent = element.subtitleResp
-        templateWeb.querySelector("#titleResp2").textContent = element.subtitleResp
-
-        templateWeb.querySelector("#desktop").textContent = element.dis1
-        templateWeb.querySelector("#tablet").textContent = element.dis2
-        templateWeb.querySelector("#movil").textContent = element.dis3
-        templateWeb.querySelector("#desktop2").textContent = element.dis1
-        templateWeb.querySelector("#tablet2").textContent = element.dis2
-        templateWeb.querySelector("#movil2").textContent = element.dis3
-
-        templateWeb.querySelector("#text1A").textContent = element.tip1A
-        templateWeb.querySelector("#text1B").textContent = element.tip1B
-        templateWeb.querySelector("#text1C").textContent = element.tip1C
-        templateWeb.querySelector("#text1D").textContent = element.tip1D
-
-        templateWeb.querySelector("#text2A").textContent = element.tip1A
-        templateWeb.querySelector("#text2B").textContent = element.tip1B
-        templateWeb.querySelector("#text2C").textContent = element.tip1C
-        templateWeb.querySelector("#text2D").textContent = element.tip2D
-
-
-        
-            */
